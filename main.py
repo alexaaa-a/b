70fb6278daa4442111add48e31e7030286140eb4
alexaaa-a <144144005+alexaaa-a@users.noreply.github.com>
2023-10-18 01:25:59 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,108 @@
+import telebot
+from telebot import custom_filters
+from telebot import StateMemoryStorage
+from telebot.handler_backends import StatesGroup, State
+
+
+state_storage = StateMemoryStorage()
+# Вставить свой токет или оставить как есть, тогда мы создадим его сами
+bot = telebot.TeleBot("6364718111:AAFIKV7_qrz-Pw974x6_Ee1mfMXSbrTOw80",
+                      state_storage=state_storage, parse_mode='Markdown')
+
+
+class PollState(StatesGroup):
+    name = State()
+    age = State()
+
+
+class HelpState(StatesGroup):
+    wait_text = State()
+
+
+text_poll = "*О лягушках*"  # Можно менять текст
+text_button_1 = "Факт 1"  # Можно менять текст
+text_button_2 = "Факт 2"  # Можно менять текст
+text_button_3 = "Факт 3"  # Можно менять текст
+text_button_3 = "Факт 4"
+text_button_3 = "Факт 5"
+
+menu_keyboard = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_poll,
+    )
+)
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_1,
+    )
+)
+
+menu_keyboard.add(
+    telebot.types.KeyboardButton(
+        text_button_2,
+    ),
+    telebot.types.KeyboardButton(
+        text_button_3,
+    )
+)
+
+
+@bot.message_handler(state="*", commands=['start'])
+def start_ex(message):
+    bot.send_message(
+        message.chat.id,
+        'Привет! хотите узнать что-то новое о *лягушках*?',  # Можно менять текст
+        reply_markup=menu_keyboard)
+
+@bot.message_handler(func=lambda message: text_poll == message.text)
+def first(message):
+    bot.send_message(message.chat.id, 'Ква! Ваше _имя_?')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.name, message.chat.id)
+
+
+@bot.message_handler(state=PollState.name)
+def name(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['name'] = message.text
+    bot.send_message(message.chat.id, 'Квуто! [Ваш](https://www.example.com/) `возраст?`')  # Можно менять текст
+    bot.set_state(message.from_user.id, PollState.age, message.chat.id)
+
+
+@bot.message_handler(state=PollState.age)
+def age(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['age'] = message.text
+    bot.send_message(message.chat.id, 'Спасибо за регистрацию!', reply_markup=menu_keyboard)  # Можно менять текст
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+@bot.message_handler(func=lambda message: text_button_1 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "Жабы - это лягушки. Слово "жаба" обычно используется для лягушек, которые имеют бородавчатую и сухую кожу, а также более короткие задние ноги. В отличие от лягушки жаба не может прыгать.", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_2 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "Техасские рогатые жабы могут стрелять кровью из уголков своих глаз.", reply_markup=menu_keyboard)  # Можно менять текст
+
+
+@bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "Некоторые ученые говорят, что после ядерного взрыва останутся только тараканы и лягушки.", reply_markup=menu_keyboard)  # Можно менять текст
+    
+    
+@bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "Лягушкам не нужно пить воду, поскольку они поглощают ее через свою кожу.", reply_markup=menu_keyboard) 
+    
+    
+    @bot.message_handler(func=lambda message: text_button_3 == message.text)
+def help_command(message):
+    bot.send_message(message.chat.id, "Самая большая лягушка в мире-это лягушка-голиаф из Камеруна в Западной Африке. Их тело может быть длиной до 30 см, а вес - более 3 кг.", reply_markup=menu_keyboard) 
+
+bot.add_custom_filter(custom_filters.StateFilter(bot))
+bot.add_custom_filter(custom_filters.TextMatchFilter())
+
+bot.infinity_polling()
+
